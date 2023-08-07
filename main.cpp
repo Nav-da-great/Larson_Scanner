9195d35d91affdf0cb5a943669d2b509e44117b4
Nav-da-great <113965179+Nav-da-great@users.noreply.github.com>
2023-08-07 16:28:27 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+int Pin[] = {13,12,11,10,9,8}; //Initializing pins
+void setup()
+{
+  int counter1;		//Creating an iterator for the loop
+  for(counter1 = 0; counter1 < 6; counter1++){
+    pinMode(Pin[counter1], OUTPUT); //Sending OUTPUT signal to respective pins
+  }
+}
+
+void loop()
+{
+  int counter2;		//Creating another iterator for the loop
+  //Creating a loop for left to right
+  for(counter2=0; counter2 < 6; counter2++){
+  	digitalWrite(Pin[counter2], HIGH);	//Sending a digital signal for 100 milliseconds
+    delay(100);
+    digitalWrite(Pin[counter2], LOW);
+  }
+  //Creating a loop for right to left
+  for(counter2=4; counter2 > -1; counter2--){
+    digitalWrite(Pin[counter2], HIGH);
+    delay(100);
+    digitalWrite(Pin[counter2], LOW);
+  }
+  delay(100);	//Delaying the overall loop by 100 milliseconds
+}
+
+//Voila! :)
